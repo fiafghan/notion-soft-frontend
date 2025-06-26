eb492e210ebf393b1861fe944090de99eb39b432
= <=>
2025-06-26 11:22:18 +0430

    aboutus-contact form completed!

--- src/about-us.tsx
+++ src/about-us.tsx
@@ -8,6 +8,7 @@ import {
   CardTitle,
 } from "@/components/ui/card"
 import { Button } from './components/ui/button';
+import Footer from './components/ui/footer';
 
 function AboutUs() {
   return (
@@ -17,7 +18,7 @@ function AboutUs() {
         <h1 className='text-md text-center text-gray-400'>Our strength is in our unparalleled 
             commitment to care for your business and its changing needs.</h1>
 
-        <div className='grid grid-cols-2 mt-10'>
+        <div className='grid grid-cols-2'>
                 <Card className='mt-10 mx-10 rounded-none shadow-none border-none'>
                         <CardHeader>
                             <CardTitle className='text-2xl text-orange-300'>Who We Are?</CardTitle>
@@ -74,7 +75,7 @@ function AboutUs() {
                        <img src="/about-us-whyChooseUs.jpg" alt="" className='w-70' />
                 </Card>
         </div>
-        <div className='grid grid-cols-1 mt-2'>
+        <div className='grid grid-cols-1 mt-20'>
                 <h1 className='text-orange-300 text-center text-3xl'>Core Values</h1>
                 <div className='grid grid-cols-3 mt-5 mx-5 px-5'>
                     <Card className='mx-3 my-3 px-5 bg-gradient-to-r from-gray-300 to-orange-300'>Integrity — Fair, transparent, and ethical conduct</Card>
@@ -84,17 +85,19 @@ function AboutUs() {
                     <Card className='mx-3 my-3 px-5 bg-gradient-to-r from-gray-300 to-orange-300'>Sustainability — Digital ecosystem for the future</Card>
                     <Card className='mx-3 my-3 px-5 bg-gradient-to-r from-gray-300 to-orange-300'>Shared Prosperity — Growth for all stakeholders</Card>
                 </div>
-                <div className='grid grid-cols-1 mt-20'>
+                <div className='grid grid-cols-1 mt-20 mb-20'>
                     <h1 className = "text-4xl text-center text-orange-300 mb-5">Contact Us</h1>
-                    <Card className='mx-100 rounded-none py-10'>
-                        <input type="text" placeholder = "Your Name" className = "mx-5 border-1 border-gray-400 px-2 text-gray-500" />
-                        <input type="text" placeholder = "Your Email" className = "mx-5 border-1 border-gray-400 px-2 text-gray-500" />
-                        <input type="text" placeholder = "Your Message" className = "mx-5 border-1 border-gray-400 px-2 text-gray-500" />
+                    <Card className='mx-100 rounded-none bg-gradient-to-r from-gray-300 via-orange-300 to-gray-200 border-none shadow-none'>
+                        <input type="text" placeholder = "Your Name" className = "mx-5 border-1 border-gray-100 px-2 text-gray-500 focus:outline-none" />
+                        <input type="text" placeholder = "Your Email" className = "mx-5 border-1 border-gray-100 px-2 text-gray-500" />
+                        <input type="text" placeholder = "Your Message" className = "mx-5 border-1 border-gray-100 px-2 text-gray-500" />
                         <div className='grid grid-cols-1 mx-5'>
-                                <Button title='Submit' className='w-full bg-gradient-to-r from-orange-300 via-gray-400 to-orange-200'>Send</Button>
+                                <Button title='Submit' className='w-full bg-gradient-to-r from-orange-300 via-gray-400 to-orange-200'>Send Message</Button>
                         </div>
                     </Card>
                 </div>
+
+                <Footer />
         </div>
     </div>
   )
